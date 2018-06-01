354a84cfee0ac5e0155a9c7af62506eee60c7cd6
juancetusjc <juancetujcpt@gmail.com>
2018-06-01 15:51:20 -0500

    Test 001 - Jenkins

--- src/main/java/es/dmunozfer/jenkins/DemoApplication.java
+++ src/main/java/es/dmunozfer/jenkins/DemoApplication.java
@@ -9,4 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	public void testF001(){
+		System.out.println("Test testF001");
+	}
 }
